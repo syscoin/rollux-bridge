9aa4c460e720008596d16ba6d3bd9c8481aeb9e4
Ted Ian Osias <osiastedian@gmail.com>
2022-05-11 22:12:01 +0800

    feat(home): initial implementation of home page

---
+++ components/theme.ts
@@ -0,0 +1,14 @@
+import { createTheme } from "@mui/material";
+
+const theme = createTheme({
+  palette: {
+    text: {
+      primary: "#1A1A28",
+    },
+    common: {
+      black: "#1A1A28",
+    },
+  },
+});
+
+export default theme;
