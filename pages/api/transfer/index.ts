364a26292aa8fd7368d8e801ba731e6364444277
Ted Ian Osias <osiastedian@gmail.com>
2022-08-23 22:13:42 +0800

    fix: add api auth for fetching list #6

--- pages/api/transfer/index.ts
+++ pages/api/transfer/index.ts
@@ -1,11 +1,20 @@
 import { NextApiHandler } from "next";
 import firebase from "firebase-setup";
 import { collection, getDocs, orderBy, query, where } from "firebase/firestore";
+import { signInWithEmailAndPassword } from "firebase/auth";
 
 const getAll: NextApiHandler = async (req, res) => {
   const { nevm, utxo } = req.query;
   if (!nevm || !utxo) {
     return res.status(400).json({ message: "Some parameters are missing" });
+  }
+
+  if (process.env.NODE_ENV !== "development" && firebase.auth) {
+    await signInWithEmailAndPassword(
+      firebase.auth,
+      process.env.FIREBASE_AUTH_EMAIL!,
+      process.env.FIREBASE_AUTH_PASSWORD!
+    );
   }
 
   const transferQuery = query(
