2952328f828f8b8a6ac07eda069ce831a52ee2d6
Ted Ian Osias <osiastedian@gmail.com>
2022-08-26 11:38:04 +0800

    chore(blocktime-disclaimer): added more info regarding transaction time #11

--- components/BlocktimeDisclaimer.tsx
+++ components/BlocktimeDisclaimer.tsx
@@ -21,11 +21,12 @@ const BlocktimeDisclaimer = () => {
         horizontal: "right",
         vertical: "bottom",
       }}
-      autoHideDuration={10000}
+      autoHideDuration={30000}
     >
       <Alert onClose={handleClose} severity="info" sx={{ width: "100%" }}>
-        Syscoin&apos;s average block time are 2.5mins long. Please keep this in
-        mind when monitoring your transactions when using this app.
+        Syscoin&apos;s average block time are 2.5 mins long and transaction time
+        may take more than 10 mins. Please keep this in mind when monitoring
+        your transactions when using this app.
       </Alert>
     </Snackbar>
   );
